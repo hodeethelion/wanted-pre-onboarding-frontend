7f30f9d1c6a7da73f47edd03c81b9350ca03cad8
hodee <tlsghk8619@gmail.com>
2023-08-07 14:56:28 +0900

    feat: 수정 기능 추가

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,8 @@ const Todo = ({ setting, refreshTodos }) => {
   const [isModifying, setIsModifying] = useState(false);
   const [isChecked, setIsChecked] = useState(setting.isCompleted);
   const [isLoading, setIsLoading] = useState(false);
+  const [modifiedInput, setModifiedInput] = useState(setting.todo);
+
   const openChange = () => {
     setIsModifying(true);
   };
@@ -45,6 +47,35 @@ const Todo = ({ setting, refreshTodos }) => {
     }
   };
 
+  const changeTodo = async (updateTodo) => {
+    try {
+      setIsLoading(true);
+      console.log("updating name of todo ");
+      console.log(updateTodo)
+      const access_token = localStorage.getItem("access_token");
+      const response = await axios.put(
+        `http://localhost:8000/todos/${setting.id}`,
+        {
+          todo: updateTodo,
+          isCompleted: setting.isCompleted,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.error("There was an error updating the todo:", error.response.data);
+    } finally {
+      refreshTodos();
+      setIsLoading(false);
+    }
+
+  }
+
+
   const deleteTodo = async () => {
     console.log("삭제");
     try {
@@ -64,8 +95,8 @@ const Todo = ({ setting, refreshTodos }) => {
     } catch (error) {
       console.log(error);
     } finally {
-      setIsLoading(false);
       refreshTodos();
+      setIsLoading(false);
     }
   };
 
@@ -74,11 +105,14 @@ const Todo = ({ setting, refreshTodos }) => {
       {isModifying ? (
         // Modify mode
         <div>
-          <input data-testid="modify-input" />
+          <input data-testid="modify-input" value={modifiedInput} onChange={(e)=>setModifiedInput(e.target.value)}/>
           <button 
             data-testid="submit-button" 
             onClick={() => {
               // Logic to submit changes
+              // 내가 하고 싶은것 눌렀을 때 새로운 todo 뜨도록 
+              console.log(modifiedInput)
+              changeTodo(modifiedInput);
               closeChange(); 
             }}>
             제출
